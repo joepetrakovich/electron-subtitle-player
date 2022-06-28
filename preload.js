6de5ab3836899db0aa8b2bdb8fe8c8148c1c1051
Joe Petrakovich <jpetrakovich@bankdirectsystems.com>
2022-06-28 17:04:24 -0500

    Added a loop that displays the timed subs

--- preload.js
+++ preload.js
@@ -1,16 +1,50 @@
 const { contextBridge } = require('electron')
-const { parse, resync, stringify } = require('subtitle')
+const { parse, filter, resync, stringify } = require('subtitle')
 const fs = require('fs')
 
 contextBridge.exposeInMainWorld('electron', {
   parseSrt: (filePath) => {
+    var cues = [];
     fs.createReadStream(filePath)
       .pipe(parse())
-      .on('data', node => {
-        console.log('parsed node:', node)
+      .pipe(filter(node => node.type === 'cue'))
+      .on('data', cue => {
+        console.log('parsed cue:', cue)
+        cues.push(cue);
       })
       .on('error', console.error)
-      .on('finish', () => console.log('parser has finished'))
+      .on('finish', () => {
+        console.log('parser has finished')
+        
+        // in your event loop, determine the current time
+        // if it is larger than the end time of the current subtitle, unpost it
+        // if it is larger than the start of the first subtitle in the list, post it and remove it from the list.
+
+        var start;
+        var currentCue = null;
+        var currentCueNumber = 0;
+        var nextCue = cues[0];
+
+        function intervalFunc() {
+          if (start === undefined) {
+            start = Date.now();
+          }
+          const elapsed = Date.now() - start;
+          //console.log(`elapsed: ${elapsed}`);
+          if (currentCue && elapsed >= currentCue.data.end) {
+            //console.log(`hiding current cue.`);
+            currentCue = null;
+          } 
+          if (nextCue && elapsed >= nextCue.data.start) {
+            currentCue = nextCue;
+            currentCueNumber++;
+            nextCue = cues.length > currentCueNumber ? cues[currentCueNumber] : null;
+            console.log(currentCue.data.text);
+          }
+        }
+        
+        setInterval(intervalFunc, 30);
+      })
   }
 })
 
